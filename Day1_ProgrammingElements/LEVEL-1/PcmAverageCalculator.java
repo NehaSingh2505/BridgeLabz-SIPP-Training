6a1b5c48ffa9f1241c06701b082ef3a7f953bb2d
NEHA SINGH <nehasingh9411450@gmail.com>
2025-06-12 13:36:16 +0530

    [Neha Singh] added PCM Average calculator program

---
+++ Day1_ProgrammingElements/LEVEL-1/PcmAverageCalculator.java
@@ -0,0 +1,17 @@
+public class PcmAverageCalculator {
+
+    public static void main(String[] args) {
+        int physicsMarks = 95;
+        int chemistryMarks = 96;
+        int mathsMarks = 94;
+
+        double averagePercentage = calculateAverage(physicsMarks, chemistryMarks, mathsMarks);
+        System.out.println("Sam’s average mark in PCM is " + averagePercentage + "%");
+    }
+
+    // Method to calculate average
+    public static double calculateAverage(int physics, int chemistry, int maths) {
+        int totalMarks = physics + chemistry + maths;
+        return totalMarks / 3.0;
+    }
+}
