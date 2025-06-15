d48a0347973476ca028aed2a05ed0e71abbb8dc9
NEHA SINGH <nehasingh9411450@gmail.com>
2025-06-15 15:17:27 +0530

    [Neha Singh] Refactored

--- ControlFlow/LEVEL-1/RocketLaunchCount.java
+++ ControlFlow/LEVEL-1/RocketLaunchCount.java
@@ -1,8 +1,4 @@
-// Write a program to count down the number from the user input value to 1 using a while loop for a rocket launch
-// Hint => 
-// Create a variable counter to take user inputted value for the countdown.
-// Use the while loop to check if the counter is 1
-// Inside a while loop, print the value of the counter and decrement the counter.
+
  import java.util.Scanner;
  public class RocketLaunchCount {
      public static void main(String[] args) {
@@ -15,7 +11,7 @@
              counter--;
          }
 
-         System.out.println("Rocket launched!");
+         System.out.println("Rocket launched");
          scanner.close();
      }
  }
